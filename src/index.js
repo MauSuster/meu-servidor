f8e4b933f379628c983f5857c324c8d394831230
Msuster <sustermauricio@gmil.com>
2025-01-31 17:57:04 -0300

    funfa

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ async function getConnection(){
 
 async function execSQLQuery(sqlQry){
     await getConnection();
-    const request = sql.Request();
+    const request = new sql.Request();
     const {recordset} = await request.query(sqlQry);
     return recordset;
 
